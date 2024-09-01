9931a482a466c33e405fbf73475bd2e94c3dbab8
lxRbckl <lxrbckl@gmail.com>
2024-09-01 03:52:24 -0500

    Update

--- app.ts
+++ app.ts
@@ -17,68 +17,68 @@ import markdownConfig from './src/configs/markdownManagerConfig';
 
 (async () => {
 
-   cron.schedule('0 0 * * *', async () => {
+   // cron.schedule('0 0 * * *', async () => {
 
-      // initialize objects <
-      const dataHandler: dataManager = new dataManager();
-      const octokitHandler: octokitManager = new octokitManager({
+   // initialize objects <
+   const dataHandler: dataManager = new dataManager();
+   const octokitHandler: octokitManager = new octokitManager({
 
-         githubUsers : await axiosGet(octokitConfig.urlGitHubUsers)
-
-      });
-      const markdownHandler: markdownManager = new markdownManager({
-
-         elementResources : await axiosGet(markdownConfig.urlElementResources),
-         elementDescriptions : await axiosGet(markdownConfig.urlElementDescriptions)
-
-      });
-
-      // >
-
-      // fetch readme and build data <
-      // publish all elements from data <
-      await octokitHandler.fetchAllReadme({
-
-         callback : ({file, hyperlink}) => {
-
-            dataHandler.addProperties({
-
-               readmeHyperlink : hyperlink,
-               readmeProperties : markdownHandler.extractProperties({file : file})
-
-            });
-
-         }
-
-      });
-
-      await octokitHandler.publishAllReadme({
-
-         data : dataHandler.getData(),
-         callback : ({
-
-            topic,
-            subject,
-            projects,
-            ecosystem
-
-         }) => {
-
-            return markdownHandler.build({
-
-               topic : topic,
-               subject : subject,
-               projects : projects,
-               ecosystem : ecosystem
-
-            });
-
-         }
-
-      });
-
-      // >
+      githubUsers : await axiosGet(octokitConfig.urlGitHubUsers)
 
    });
+   const markdownHandler: markdownManager = new markdownManager({
+
+      elementResources : await axiosGet(markdownConfig.urlElementResources),
+      elementDescriptions : await axiosGet(markdownConfig.urlElementDescriptions)
+
+   });
+
+   // >
+
+   // fetch readme and build data <
+   // publish all elements from data <
+   await octokitHandler.fetchAllReadme({
+
+      callback : ({file, hyperlink}) => {
+
+         dataHandler.addProperties({
+
+            readmeHyperlink : hyperlink,
+            readmeProperties : markdownHandler.extractProperties({file : file})
+
+         });
+
+      }
+
+   });
+
+   await octokitHandler.publishAllReadme({
+
+      data : dataHandler.getData(),
+      callback : ({
+
+         topic,
+         subject,
+         projects,
+         ecosystem
+
+      }) => {
+         
+         return markdownHandler.build({
+
+            topic : topic,
+            subject : subject,
+            projects : projects,
+            ecosystem : ecosystem
+
+         });
+
+      }
+
+   });
+
+   // >
+
+   // });
 
 })();
